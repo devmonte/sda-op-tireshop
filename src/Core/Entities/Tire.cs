db29ab3f4ccd71fb20639e421ba43b538a660452
devmonte <greg.jonca@gmail.com>
2019-09-02 14:11:26 +0200

    Add simple view with view component to show tires

---
+++ src/Core/Entities/Tire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Tire
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Producer { get; set; }
+        public int WarrantyInYears { get; set; }
+        public int VMax { get; set; }
+    }
+}
---
+++ src/TireShop/Controllers/TiresController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class TiresController : Controller
+    {
+        public IActionResult Tires()
+        {
+            var tires = new List<Tire>();
+            tires.Add(new Tire
+            {
+                Name = "TestTire1", 
+                Type = "Winter",
+                Producer = "Dunlop",
+                VMax = 195,
+                WarrantyInYears = 3
+            });
+            tires.Add(new Tire
+            {
+                Name = "TestTire2",
+                Type = "Winter",
+                Producer = "Dunlop",
+                VMax = 195,
+                WarrantyInYears = 3
+            });
+            tires.Add(new Tire
+            {
+                Name = "TestTire3",
+                Type = "Winter",
+                Producer = "Dunlop",
+                VMax = 195,
+                WarrantyInYears = 3
+            });
+            return View(tires);
+        }
+
+        public IActionResult Manufacturers()
+        {
+            return View();
+        }
+
+    }
+}
